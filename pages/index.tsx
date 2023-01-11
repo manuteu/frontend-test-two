f26c30d88f5dbe1c4de30ecefbdf1e51f634c15b
Matheus <matheusim27@hotmail.com>
2023-01-11 19:55:50 -0300

    add: initial structure

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+import Card from './components/Card'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Card />
+      <h1>Teste Front end</h1>
+    </div>
+  )
+}
